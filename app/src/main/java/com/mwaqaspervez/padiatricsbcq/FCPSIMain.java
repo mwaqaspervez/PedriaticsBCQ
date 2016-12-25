75fabc1586db0cd868af3c0061aa1262bbebe976
Muhammad Waqas <m.waqaspervez@gmail.com>
2016-12-26 00:11:58 +0500

    desgined layouts

---
+++ app/src/main/java/com/mwaqaspervez/padiatricsbcq/FCPSIMain.java
@@ -0,0 +1,19 @@
+package com.mwaqaspervez.padiatricsbcq;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Muhammad Waqas on 12/24/2016.
+ */
+
+public class FCPSIMain extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_fcps_i);
+    }
+}
---
+++ app/src/main/java/com/mwaqaspervez/padiatricsbcq/MBBSMain.java
@@ -0,0 +1,16 @@
+package com.mwaqaspervez.padiatricsbcq;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MBBSMain extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_mbbs);
+    }
+}
