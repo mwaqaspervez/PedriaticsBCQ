27338b2e24509f7bf171e0da230f723e7660343a
mwaqaspervez <m.waqaspervez@gmail.com>
2016-12-26 18:24:54 +0500

    added login

--- app/src/main/java/com/mwaqaspervez/padiatricsbcq/MainActivity.java
+++ app/src/main/java/com/mwaqaspervez/padiatricsbcq/MainActivity.java
@@ -1,11 +1,16 @@
 package com.mwaqaspervez.padiatricsbcq;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -40,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_left);
                 break;
 
-
             case R.id.fcps_ii:
                 startActivity(new Intent(this, FCPSIIMain.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_left);
@@ -59,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
 
 
             case R.id.register:
-
                 if (ApplicationClass.getInstance().getCurrentUser() != null) {
                     ApplicationClass.getInstance().getAuth().signOut();
                     finish();
@@ -69,9 +78,16 @@ public class MainActivity extends AppCompatActivity {
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_left);
                 }
                 break;
-
-
             case R.id.books:
+                String packageName = "";
+                Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+                if (intent == null) {
+                    // Bring user to the market or let them choose an app?
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("market://details?id=" + packageName));
+                }
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
                 break;
         }
     }
